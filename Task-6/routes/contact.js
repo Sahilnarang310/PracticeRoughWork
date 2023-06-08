0388f7bbd27e413292d2b79569b2f3dfb097ae63
Sahilnarang310 <softwaredev310@gamil.com>
2023-06-08 23:11:00 +0530

    Revised Task 6 in express framework  contactus, success controllers added

---
+++ Task-6/routes/contact.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+const contactusController = require('../controllers/contact');
+
+
+const router = express.Router();
+
+router.get('/contactus', contactusController.getContactUs);
+
+router.get('/success', contactusController.success);
+
+module.exports = router;
+
